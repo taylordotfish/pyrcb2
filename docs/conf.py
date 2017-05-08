bce7760e0e4fbbc93fe69d2e9517a63a05c22154
taylor.fish <contact@taylor.fish>
2017-05-07 19:02:34 -0700

    Update copyright date in docs

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyrcb2'
-copyright = ' 2015-2016 taylor.fish'
+copyright = ' 2015-2017 taylor.fish'
 author = 'taylor.fish'
 
 # The version info for the project you're documenting, acts as replacement for
