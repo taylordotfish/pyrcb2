ac1cb150b6b8064b237698e2deb0c56be1c43e85
taylor.fish <contact@taylor.fish>
2018-04-16 23:16:42 -0700

    Bump dev version

--- pyrcb2/__init__.py
+++ pyrcb2/__init__.py
@@ -37,7 +37,7 @@ from . import messages
 from . import numerics
 from . import utils
 
-__version__ = "0.5.3"
+__version__ = "0.5.4-dev"
 
 # Silence Pyflakes warnings about unused imports.
 assert [Status]
