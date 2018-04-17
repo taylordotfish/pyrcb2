4b54964847e3029db0c4578e69f2b0790fcc1a25
taylor.fish <contact@taylor.fish>
2018-04-16 23:15:01 -0700

    Version 0.5.3

--- pyrcb2/__init__.py
+++ pyrcb2/__init__.py
@@ -37,7 +37,7 @@ from . import messages
 from . import numerics
 from . import utils
 
-__version__ = "0.5.3-dev"
+__version__ = "0.5.3"
 
 # Silence Pyflakes warnings about unused imports.
 assert [Status]
